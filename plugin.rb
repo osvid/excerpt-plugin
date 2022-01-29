9258f25bed88e1d53d5dee15783d59261f49ddee
osvid <oscardavidsanchez@gmail.com>
2022-01-29 12:02:30 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# name: excerpt-plugin
+# about: A super simple plugin to customize excerpts
+# version: 0.0.1
+# authors: osvid
+
+after_initialize do
+
+  # change excerpt format
+  class ::Post
+    module ExcerptForTopicPoemame
+      def excerpt_for_topic
+        Post.excerpt(cooked, 500, {keep_newlines: true, strip_links: true})
+      end
+    end
+    prepend ExcerptForTopicPoemame
+  end
